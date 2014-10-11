fd2e20bf7e634e0799689220c42fe55f2af5f225
Andrew Buckingham <abuckingham99@gmail.com>
2014-10-11 16:15:24 -0500

    complete 'Basics' Problems 1 and 2

---
+++ 02_basics/solution1.rb
@@ -0,0 +1,12 @@
+# #1: My original, (overly?) simple solution
+puts "Andrew " + "Buckingham" # Andrew Buckingham
+
+# #2: From the books's solution; assigns variables and then concatenates them:
+
+fname = "Andrew"
+lname = "Buckingham"
+
+puts fname + " " + lname
+
+# #3: From the book's solution; using string interpolation:
+puts "#{fname} #{lname}"

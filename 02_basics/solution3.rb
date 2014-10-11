50abe4b4110aa20c3163eb2844244fd08d6b2b92
Andrew Buckingham <abuckingham99@gmail.com>
2014-10-11 17:42:00 -0500

    fix link for solution 2 and output for solution 3

--- 02_basics/solution3.rb
+++ 02_basics/solution3.rb
@@ -1,11 +1,11 @@
 movies = {:The_Empire_Strikes_Back =>'1980',
-  :Into_the_Fire => '2010',
+  :The_Dark_Knight => '2008',
   :Avatar => '2009',
   :The_Blind_Side => '2009',
-  :The_Dark_Knight => '2008'}
+  :Into_the_Fire => '2010'}  
 
 puts movies[:The_Empire_Strikes_Back]
-puts movies[:Into_the_Fire]
+puts movies[:The_Dark_Knight]
 puts movies[:Avatar]
 puts movies[:The_Blind_Side]
-puts movies[:The_Dark_Knight]
+puts movies[:Into_the_Fire]

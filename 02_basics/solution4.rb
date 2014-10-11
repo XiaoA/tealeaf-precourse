9a675995c1ae971709c9613b82343331b24c1226
Andrew Buckingham <abuckingham99@gmail.com>
2014-10-11 18:21:19 -0500

    complete 'Basics' solutions 4 and 5

--- 02_basics/solution4.rb
+++ 02_basics/solution4.rb
@@ -1,2 +1,12 @@
-years = [1975, 2004, 2013, 2001, 1981]
+# My solution
+years = [1980, 2008, 2009, 2009, 2010]
 puts years
+
+# Book solution
+dates = [1980, 2008, 2009, 2009, 2010]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
---
+++ 02_basics/solution5.rb
@@ -0,0 +1,10 @@
+# Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
+five = 5 * 4 * 3 * 2 * 1
+six = 6 * 5 * 4 * 3 * 2 * 1
+seven = 7 * 6 * 5 * 4 * 3 * 2 * 1
+eight = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts five
+puts six
+puts seven
+puts eight

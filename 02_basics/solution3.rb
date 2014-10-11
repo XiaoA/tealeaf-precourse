7c129544278408d8471644c51a81e4c59cd80f6d
Andrew Buckingham <abuckingham99@gmail.com>
2014-10-11 16:32:41 -0500

    complete 'Basics' solution 3

---
+++ 02_basics/solution3.rb
@@ -0,0 +1,11 @@
+movies = {:The_Empire_Strikes_Back =>'1980',
+  :Into_the_Fire => '2010',
+  :Avatar => '2009',
+  :The_Blind_Side => '2009',
+  :The_Dark_Knight => '2008'}
+
+puts movies[:The_Empire_Strikes_Back]
+puts movies[:Into_the_Fire]
+puts movies[:Avatar]
+puts movies[:The_Blind_Side]
+puts movies[:The_Dark_Knight]

bc19121d7d3398a8c62b4930389371ed39ea1bb4
Andrew Buckingham <abuckingham99@gmail.com>
2014-10-15 22:18:07 -0500

    add book's answer for exercise 3

--- 06_loops/exercise3.rb
+++ 06_loops/exercise3.rb
@@ -1,5 +1,5 @@
 # Lists Japanese Imperial Reign names since the end of the Edo Period.
 
 ["Meiji", "Taisho", "Showa", "Heian"].each_with_index do |item, index |
-     puts "#{index}: #{item}"
+     puts "#{index + 1}: #{item}"
    end

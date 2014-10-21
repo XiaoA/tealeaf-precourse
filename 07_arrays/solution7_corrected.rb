fc576e61e3e61e528dc3ae44b7fedda7caf4287b
Andrew Buckingham <abuckingham99@gmail.com>
2014-10-20 22:08:41 -0500

    Finish 'Arrays' section

---
+++ 07_arrays/solution7_corrected.rb
@@ -0,0 +1,9 @@
+hachi = [1, 2, 3, 4, 5, 6, 7, 8]
+  hachi2 = []
+
+  hachi.each do |num|
+    hachi2 << num + 2
+  end
+
+p hachi
+p hachi2
---
+++ 07_arrays/solution_7.rb
@@ -0,0 +1,10 @@
+#07_arrays/solution_7.rb
+  # increments array by two, and prints both with "p" method
+  
+  hachi = [1, 2, 3, 4, 5, 6, 7, 8]
+  
+  hachi2 = hachi.map { |num| num+2 }
+
+
+p hachi
+p hachi2

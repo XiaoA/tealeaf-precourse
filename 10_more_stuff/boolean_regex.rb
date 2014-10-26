256b8cc3e573f034c9989491456be9e81265062a
Andrew Buckingham <abuckingham99@gmail.com>
2014-10-26 10:59:42 -0500

    Add 'booleand_matchdata.rb' and 'boolean_regex.rb'

--- 10_more_stuff/boolean_regex.rb
+++ 10_more_stuff/boolean_regex.rb
@@ -1,11 +1,11 @@
 # 10_more_stuff/boolean_regex.rb
 
 def has_a_b?(string)
-if string =~ /b/
-puts "We have a match!"
-else 
-puts "No match here."
-end
+  if string =~ /b/
+    puts "We have a match!"
+  else 
+    puts "No match here."
+  end
 end
 
 has_a_b?("basketball")

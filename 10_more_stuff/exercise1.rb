218ca75e60dca291e7f530e1ebffd638a08e7c88
Andrew Buckingham <abuckingham99@gmail.com>
2014-10-27 06:30:34 -0500

    Update 'More Stuff' exercise 1

--- 10_more_stuff/exercise1.rb
+++ 10_more_stuff/exercise1.rb
@@ -11,3 +11,19 @@ has_lab?("experiment")
 has_lab?("Pans Labyrinth")
 has_lab?("elaborate")
 has_lab?("polar bear")
+
+# 10_more_stuff/exercise1.rb
+  
+  def has_lab?(string)
+    if string =~ /lab/
+      puts "'#{string}' has 'lab' so it makes the cut."
+    else
+      puts "Sorry, no match."
+    end
+  end
+
+has_lab?("laboratory")
+has_lab?("experiment")
+has_lab?("Pans Labyrinth")
+has_lab?("elaborate")
+has_lab?("polar bear")

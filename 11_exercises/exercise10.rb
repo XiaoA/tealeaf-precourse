0f1895ee6168b94866302587a47b502d24a7bfde
Andrew Buckingham <abuckingham99@gmail.com>
2014-10-27 22:25:33 -0500

    Complete Exercise 11

---
+++ 11_exercises/exercise10.rb
@@ -0,0 +1,10 @@
+# 11_exercises/exercise10.rb
+# "Can hash values be arrays? Can you have an array of hashes? (give examples)"
+
+# Yes. hash values can be arrays. For example:
+
+hash_arr = {men: ['Hiro', 'Makoto', 'Shinya'], women: ['Ayumi', 'Eri', 'Nana']}
+
+# Yes, you can have an array of hashes. For example:
+
+arr_hash = [{name: 'David'}, {age: 32}, {occupation: 'accountant'}]

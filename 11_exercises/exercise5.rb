6aa81ef1383c198f5718b866fd22730a53f46260
Andrew Buckingham <abuckingham99@gmail.com>
2014-10-27 20:48:03 -0500

    Complete Exercise 5

---
+++ 11_exercises/exercise5.rb
@@ -0,0 +1,13 @@
+# 11_exercises/exercise 5.rb
+# Get rid of "11". And append a "3".
+
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+# Remove 11
+arr.pop
+
+# Append 3
+arr.push(3)
+# or: 
+# arr << 3
+p arr
